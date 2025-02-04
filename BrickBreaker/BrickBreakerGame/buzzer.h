028e2d64df68c81458296b2716387bcfa4d140dc
Jayflo44 <123122247+Jayflo44@users.noreply.github.com>
2025-02-04 16:55:31 -0700

    Delete BrickBreaker directory

--- BrickBreaker/BrickBreakerGame/buzzer.h
+++
@@ -1,7 +0,0 @@
-#ifndef BUZZER_INCLUDED
-#define BUZZER_INCLUDED
-
-void buzzer_init();
-void buzzer_set_period(short cycles);
-
-#endif // BUZZER_INCLUDED
--- BrickBreaker/BrickBreakerGame/led.h
+++
@@ -1,8 +0,0 @@
-#ifndef LED_INCLUDED
-#define LED_INCLUDED
-
-#define LED_RED BIT6  // P1.6
-#define LED_GREEN BIT0  // P1.0
-#define LEDS (BIT0 | BIT6)
-
-#endif // LED_INCLUDED
